6412ea5d7363982a998c18429304a27276b8bd7d
David Carabott <david@carabott.me>
2017-01-18 19:41:45 +0000

    Tidy up for -AzureLab Pester tests

--- functions/AzureLab.Tests.ps1
+++ functions/AzureLab.Tests.ps1
@@ -2,7 +2,7 @@
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
-Describe "New-AzureLab" {
+Describe "AzureLab Tests" {
 
     # Parameter Variables
     $labName = "PesterTest"
@@ -17,7 +17,7 @@ Describe "New-AzureLab" {
         #TODO - Return Data
     } 
 
-    Context "Input" {
+    Context "New-AzureLab Tests" {
         # TestCases
         $labNameCases = @(
             @{
@@ -33,24 +33,22 @@ Describe "New-AzureLab" {
         # Parameter Variables
         $badAzureLocation = "Biggleswade"
 
-        It "accepts valid input for LabName: <TestScenario>" -TestCases $labNameCases {
+        It "Input - accepts valid input for LabName: <TestScenario>" -TestCases $labNameCases {
             {New-AzureLab -LabName $testLabName -AzureLocation $azureLocation -LabPassword $password} |
                 should throw "Cannot validate argument on parameter"
         }
 
-        It "accepts existing Azure regions as input for AzureLocation" {
+        It "Input - accepts existing Azure regions as input for AzureLocation" {
             {New-AzureLab -LabName $labName -AzureLocation $badAzureLocation -LabPassword $password} |
                 should throw "Cannot validate argument on parameter"
         }
 
-        It "does not accept invalid Azure regions as input for AzureLocation" {
+        It "Input - does not accept invalid Azure regions as input for AzureLocation" {
             {New-AzureLab -LabName $labName -AzureLocation $badAzureLocation -LabPassword $password} |
                 should throw "Cannot validate argument on parameter"
         }
-    }
 
-    Context Execution {
-        It "Does not re-create ResourceGroup if already exists" {
+        It "Execution - Does not re-create ResourceGroup if already exists" {
             Mock -CommandName Get-AzureRmResourceGroup -Verifiable -MockWith {
                 $true
             }
@@ -58,22 +56,20 @@ Describe "New-AzureLab" {
             Assert-MockCalled New-AzureRmResourceGroup -times 0
         }
 
-        It "Creates the required ResourceGroup if it does not exist" {
+        It "Exectution - Creates the required ResourceGroup if it does not exist" {
             Mock -CommandName Get-AzureRmResourceGroup -Verifiable -MockWith {
                 $false
             }
             New-AzureLab -LabName $labName -AzureLocation $azureLocation -LabPassword $password
             Assert-MockCalled New-AzureRmResourceGroup -Times 1
         }        
-    }
 
-    Context Output {
-        It -Pending "Returns the proper type"
+        It -Pending "Output - Returns the proper type"
 
     }
-}
 
-Describe "Remove-AzureLab" {
+    Context "Remove-AzureLab Tests" {
+
   # TestCases
   $labNameCases = @(
       @{
@@ -104,15 +100,12 @@ Describe "Remove-AzureLab" {
     Return $true
   } -ParameterFilter {$LabName -eq $LabName}
 
-  Context Input {
-    It "accepts valid input for LabName: <TestScenario>" -TestCases $labNameCases {
+    It "Input - accepts valid input for LabName: <TestScenario>" -TestCases $labNameCases {
       {New-AzureLab -LabName $testLabName -AzureLocation $azureLocation -LabPassword $password} |
         should throw "Cannot validate argument on parameter"
     }
-  }
 
-  Context Execution {
-    It "should remove a specified Resource Group" {
+    It "Execution - should remove a specified Resource Group" {
         $returnData = @{
             ResourceGroupName = $LabName
             Tags = @{
@@ -123,14 +116,14 @@ Describe "Remove-AzureLab" {
         Assert-MockCalled Remove-AzureRmResourceGroup -Times 1
     }
 
-    It "should throw if the specified Resource Group does not exist" {
+    It "Execution - should throw if the specified Resource Group does not exist" {
         $LabName = "DoesNotExist"
         $returnData = $null
         {Remove-AzureLab -LabName $LabName} |
           Should throw "Cannot remove Resource Group $LabName - it does not exist."
     }
     
-    It "should throw if specified Resource Group does not have appropriate tag 'AutoLab'" {
+    It "Execution - should throw if specified Resource Group does not have appropriate tag 'AutoLab'" {
         $returnData = @{
                 ResourceGroupName = $LabName
                 Tags = @{}
@@ -138,15 +131,13 @@ Describe "Remove-AzureLab" {
         {Remove-AzureLab -LabName $LabName} |
             Should throw "Cannot remove Resource Group $LabName - does not have the correct tag, 'AutoLab'."
     }
-  }
 
-  Context Output {
-    It "Returns $false if ResourceGroup does exist after remove attempt" {
+    It "Output - Returns $false if ResourceGroup does exist after remove attempt" {
     Remove-AzureLab -LabName $LabName | Should be $false
       Assert-VerifiableMocks
     }
 
-    It "should return $true if ResourceGroup does not exist after remove attempted" {
+    It "Output - should return $true if ResourceGroup does not exist after remove attempted" {
       Mock -CommandName Get-AzureRmResourceGroup -MockWith {
         Return $Null
       } -ParameterFilter {$Name -and $Name -eq $LabName} 
